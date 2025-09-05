5b0c3b46853480f8d1cc62a2a18fde8b546a598a
Josue123212 <choquepumajosue@gmail.com>
2025-09-04 23:11:24 -0500

    Terminado la implementacion de clases

--- src/Biblioteca/Main.kt
+++ src/Biblioteca/Main.kt
@@ -1,4 +1,22 @@
-package Biblioteca
+package biblioteca
 
-class Main {
+fun main() {
+    // Crear libros
+    val libro1 = LibroDigital("Kotlin Avanzado", "Juan Pérez", 200, 5.6)
+    val libro2 = LibroFisico("Algoritmos", "María López", 350, 800)
+
+    libro1.mostrarInfo()
+    libro2.mostrarInfo()
+
+    // Crear usuarios
+    val estudiante = EstudiantePrestamo("Ana")
+    val profesor = ProfesorPrestamo("Carlos")
+
+    // Préstamos
+    estudiante.prestar(libro1)
+    estudiante.devolver(libro1)
+
+    profesor.prestar(libro2)
+    profesor.devolver(libro2)
 }
+//Tarea terminada
